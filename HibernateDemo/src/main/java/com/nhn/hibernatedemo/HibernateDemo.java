25c5843cb6fb5f6dc0059ad4833ffa097f65bb6e
nhat200901@gmail.com <nhat200901@gmail.com>
2023-02-18 15:20:29 +0700

    1

---
+++ HibernateDemo/src/main/java/com/nhn/hibernatedemo/HibernateDemo.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.nhn.hibernatedemo;
+
+import javax.persistence.Query;
+import org.hibernate.Session;
+
+/**
+ *
+ * @author admin
+ */
+public class HibernateDemo {
+
+    public static void main(String[] args) {
+        Session s = HibernateUtils.getFactory().openSession();
+        
+        Query q = s.createQuery("From Category");
+        
+        s.close();
+    }
+}
